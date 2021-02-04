797c74d2a34a38a109ebb31441da05f524206b8b
Dmitriy Leshchev <LeshchevDmitriy@ya.ru>
2021-02-05 01:30:43 +0300

    add routing and new empty components(dialogs,news,music, settings

--- socialNetwork/src/components/Navbar/Navbar.jsx
+++ socialNetwork/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './Navbar.module.css';
+
 const Navbar = () => {
   return (
     <nav className={s.nav}>
         <ul>
-          <li><a className={s.item}  href="#">Profile</a></li>
-          <li><a className={s.item} href="#">Messages</a></li>
-          <li><a className={s.item} href="#">News</a></li>
-          <li><a className={s.item} href="#">Music</a></li>
-          <li><a className={s.item} href="#">Settings</a></li>
+          <li><a className={s.item}  href="/profile">Profile</a></li>
+          <li><a className={s.item} href="/dialogs">Messages</a></li>
+          <li><a className={s.item} href="/news">News</a></li>
+          <li><a className={s.item} href="/music">Music</a></li>
+          <li><a className={s.item} href="/settings">Settings</a></li>
         </ul>
       </nav>
   )
